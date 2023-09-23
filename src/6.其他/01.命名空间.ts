5bcf89bd2b563cc0a2ee4293f1628c7fffcd7929
leizh25 <leizhuzheng@qq.com>
2023-09-23 20:38:38 +0800

    命名空间

---
+++ src/6.其他/01.命名空间.ts
@@ -0,0 +1,13 @@
+export { }
+/* 
+    命名空间(namespace)
+        - 为了在模块化系统不完整时的一个补充
+*/
+namespace MyModule {
+    let a = 10
+    export let b = "hello"
+    export function fn(){}
+}
+
+MyModule.b
+
